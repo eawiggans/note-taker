df7a3310bf9775459b88b2f3e319d2d6b8ee99d3
Emma Wiggans <ea.wiggans@gmail.com>
2022-12-15 21:58:00 -0500

    all working

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,15 +1,15 @@
 const api = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend } = require('../../helpers/fsUtils');
+const uuid = require('../../helpers/uuid');
 
 // GET Route for retrieving all the notes
-api.get('/notes', (req, res) => {
+api.get('/', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new note
-api.post('/notes', (req, res) => {
+api.post('/', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   console.log(req.body);
 
